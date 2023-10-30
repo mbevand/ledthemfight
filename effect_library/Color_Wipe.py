dee5b7108336d4234a53d076f5d929af141d818f
Marc Bevand <m@zorinaq.com>
2023-10-30 09:22:44 -0700

    Color_Wipe: animate faster on longer pixel strings

--- effect_library/Color_Wipe.py
+++ effect_library/Color_Wipe.py
@@ -1,5 +1,8 @@
 colors = (black, red, orange, yellow, lime, cyan, blue, magenta)
 color_i = 0
+# the animation will move 1 pixel/frame for strings of length 0-60,
+# 2 pixels/frame for 61-120, 3 pixels/frame for 121-180, etc
+speed = 1 + int((num_pixels - 1) / 60)
 
 def before_frame(frame):
     global color_i
@@ -7,5 +10,5 @@ def before_frame(frame):
         color_i += 1
 
 def render(index, frame):
-    i = color_i if index < (frame % num_pixels) else color_i - 1
+    i = color_i if index < ((frame * speed) % num_pixels) else color_i - 1
     return colors[i % len(colors)]
