19de2bd10dde7956348ccb24a3855346da0c72dc
Marc Bevand <m@zorinaq.com>
2023-11-08 23:24:49 -0800

    Random: slow down the effect

--- effect_library/Random.py
+++ effect_library/Random.py
@@ -1,5 +1,5 @@
 from random import random, seed
 
 def render(index, frame):
-    seed(int(frame / 2) * num_pixels + index)
+    seed(int(frame / 10) * num_pixels + index)
     return hsv(random(), 1, 1)
