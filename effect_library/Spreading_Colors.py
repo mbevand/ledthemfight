537802f383f047313c3cbe99755f02674b01a6f2
Marc Bevand <m@zorinaq.com>
2023-11-10 21:59:56 -0800

    Spreading_Colors: simplification

--- effect_library/Spreading_Colors.py
+++ effect_library/Spreading_Colors.py
@@ -1,33 +1,28 @@
 import math
-from array import array
 
-buf = None
+# color spots, from the outer ones to the inner ones; note the outer ones
+# will be rendered in the foreground and will eclipse the inner ones
 colors = ('#822d08', '#ec4b07', '#f9f697', '#fcd317', '#f7a72a')
-# distance in pixels of each spot from the center of the string
-dists = [0] * len(colors)
-# radius in pixels of each color spot
-spotr = round(.06 * num_pixels)
-span = .45
-period = 60 * 6
+# distance in nr. of pixels between the center of the LED string and the
+# outer boundary of the color spot
+boundaries = [0] * len(colors)
+# width of one half of the animation, relative to the length of the LED string
+span = .5
+# width in pixels of each color spot
+spotw = round(span / len(colors) * num_pixels)
+# nr. of frames during which the spots spread, or contract
+period = 60 * 5
 
 def before_frame(frame):
-    global buf
-    buf = array('l', [-1] * num_pixels)
-    f = abs(math.cos((frame % (period * 2)) / period * math.pi) / 2 - .5)
+    # f goes from 0, to 1, back to 0, and so on, slowing down when reversing
+    f = (math.sin((frame / period - .5) * math.pi) + 1) / 2
     nr = len(colors)
     for i in range(nr):
-        dists[i] = round(num_pixels * f * span * (nr - i - 1) / (nr - 1))
-    center = round(num_pixels / 2)
-    for i in range(nr)[::-1]:
-        for j in range(spotr):
-            for half in (-1, 1):
-                for rhalf in (-1, 1):
-                    buf_i = center + dists[i] * half + j * rhalf
-                    if buf_i < len(buf):
-                        buf[buf_i] = i
+        boundaries[i] = round(num_pixels * f * span * (nr - i) / nr)
 
 def render(index, frame):
-    if buf[index] == -1:
-        return black
-    else:
-        return colors[buf[index]]
+    d = abs(index - num_pixels / 2)
+    for i in range(len(colors)):
+        if boundaries[i] >= d >= boundaries[i] - spotw:
+            return colors[i]
+    return black
