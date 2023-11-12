ceaba4273b282dd2b11e574e8fb90074e308e146
Marc Bevand <m@zorinaq.com>
2023-11-11 17:08:52 -0800

    reformatting

--- effect_library/Stripes_Moving.py
+++ effect_library/Stripes_Moving.py
@@ -1,6 +1,5 @@
 def render(index, frame):
     n = max(4, round(num_pixels / 20))
     colors = [purple] * n + [black] * 2 \
-           + [orange_halloween] * n + [black] * 2 \
-           ;
+           + [orange_halloween] * n + [black] * 2
     return colors[(index + int(-frame / 5)) % len(colors)]
